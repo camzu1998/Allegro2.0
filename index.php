cad32611c9feba1427cd0f564faa4dfaff0c1cd2
camzu1998 <kamillanger4@gmail.com>
2017-09-12 18:56:06 +0200

    First everything ok!

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Strona główna</title>
+        <link href="https://fonts.googleapis.com/css?family=Pacifico|Raleway" rel="stylesheet">
+        <link rel="stylesheet" href="index.css"/>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/skeleton.css">
+        <script src="mainscript.js"></script>
+    </head>
+
+    <body>
+        <header></header>
+    </body>
+</html>
