c537fe77250a0daa664b370b675f818ae584cb81
camzu1998 <kamillanger4@gmail.com>
2017-09-12 19:16:06 +0200

    zmiany zmiany zmiany

--- index.php
+++ index.php
@@ -8,10 +8,31 @@
         <link rel="stylesheet" href="index.css"/>
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/skeleton.css">
+        <script src="jquery-3.2.1.min.js"></script>
         <script src="mainscript.js"></script>
     </head>
 
     <body>
-        <header></header>
+        <div class="container">
+            <div class="row header">
+                <h2>Sklep blablabla</h2>
+            </div>
+            <div class="row">
+                <div class="four columns item"></div>
+                <div class="four columns item"></div>
+                <div class="four columns item"></div>
+            </div>
+            <div class="row">
+                <div class="four columns item"></div>
+                <div class="four columns item"></div>
+                <div class="four columns item"></div>
+            </div>
+            <div class="row">
+                <div class="four columns item"></div>
+                <div class="four columns item"></div>
+                <div class="four columns item"></div>
+            </div>
+
+        </div>
     </body>
 </html>
